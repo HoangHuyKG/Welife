38f639421cf16539161ff5330c37084f4ac4ffef
unknown <hoanghuywww10@gmail.com>
2022-02-24 21:54:44 +0700

    Updates

---
+++ precache-manifest.a7372191ea571eb8bc2a009d130de834.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4120d0ae35a2ea10f71b91e7936bebf",
+    "url": "/WELIFEPROJECT/Wesushi/index.html"
+  },
+  {
+    "revision": "78e8fbe50569f15d895e",
+    "url": "/WELIFEPROJECT/Wesushi/static/css/main.fc5dfaa2.chunk.css"
+  },
+  {
+    "revision": "55907a7cb973491e0047",
+    "url": "/WELIFEPROJECT/Wesushi/static/js/2.b7df0148.chunk.js"
+  },
+  {
+    "revision": "a90bb751b102ed9d8f9443a23ededd32",
+    "url": "/WELIFEPROJECT/Wesushi/static/js/2.b7df0148.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3d58b8aa31344748448",
+    "url": "/WELIFEPROJECT/Wesushi/static/js/3.fc39b07c.chunk.js"
+  },
+  {
+    "revision": "78e8fbe50569f15d895e",
+    "url": "/WELIFEPROJECT/Wesushi/static/js/main.de510be4.chunk.js"
+  },
+  {
+    "revision": "08aa8a82fcc75f554e29",
+    "url": "/WELIFEPROJECT/Wesushi/static/js/runtime-main.230d2720.js"
+  }
+]);
